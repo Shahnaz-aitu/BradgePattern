f35b7e44070e0885150205f2f2e92ef50b47069e
Shahnaz-aitu <240843@astanait.edu.kz>
2025-09-30 16:39:34 +0500

    cc

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Brand brand1= new Dior(new Cream(),new Lipstick());
-        brand1.getCosmetics();
-        Brand brand2= new Chanel(new Cream(),new Lipstick());
-        brand2.getCosmetics();
+        Brand dior= new Dior(new Cream(),new Lipstick());
+        dior.getCosmetics();
+        Brand chanel= new Chanel(new Cream(),new Lipstick());
+        chanel.getCosmetics();
     }
 }
